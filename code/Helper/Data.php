fe10d25188c02c990a5957cd836321ea58752c43
Yaroslav Rogoza <rogyar@gmail.com>
2016-03-28 10:26:54 +0200

    Delete customer by email logic

--- code/Helper/Data.php
+++ code/Helper/Data.php
@@ -10,4 +10,26 @@ class Atwix_AcceptanceHelper_Helper_Data extends Mage_Core_Helper_Abstract
         Mage::app()->cleanCache(); // Clean Magento caches
         Mage::app()->getCacheInstance()->flush(); // Clean cache storage
     }
+
+    /**
+     * Attempts to find customer by email. If customer has been found - removes it
+     * 
+     * @param string $customerEmail
+     * @return bool
+     * @throws Exception
+     */
+    public function removeCustomer($customerEmail)
+    {
+        $customer = Mage::getModel('customer/customer');
+        $customer->setWebsiteId(Mage::app()->getStore()->getWebsiteId());
+        $customer->loadByEmail($customerEmail);
+        if ($customer->getId()) {
+            Mage::register('isSecureArea', true);
+            $customer->delete();
+            return true;
+            
+        } else {
+            return false;
+        }
+    }
 }
--- code/controllers/IndexController.php
+++ code/controllers/IndexController.php
@@ -5,6 +5,7 @@
 class Atwix_AcceptanceHelper_IndexController extends Mage_Core_Controller_Front_Action
 {
     const SUCCESS_MESSAGE = '<h4 style="color: green">All operations were completed successfully</h4>';
+    const FAIL_MESSAGE = '<h4 style="color: red">There was error processing your request: %s</h4>';
 
     /** @var  Atwix_AcceptanceHelper_Helper_Data */
     protected $generalHelper;
@@ -23,5 +24,25 @@ class Atwix_AcceptanceHelper_IndexController extends Mage_Core_Controller_Front_
     {
         $this->generalHelper->clearCache();
         $this->getResponse()->setBody(self::SUCCESS_MESSAGE);
+    }
+
+    /**
+     * Initialises user removing by provided email
+     */
+    public function removecustomerAction()
+    {
+        $customerEmail = $this->getRequest()->getParam('email');
+        if (empty($customerEmail)) {
+            $this->getResponse()->setBody(sprintf(self::FAIL_MESSAGE, 'No customer email specified'));
+            return $this;
+        }
+
+        if (!$this->generalHelper->removeCustomer($customerEmail)) {
+            $this->getResponse()->setBody(sprintf(self::FAIL_MESSAGE, 'Customer with provided email cannot be found'));
+            return $this;
+        }
+
+        $this->getResponse()->setBody(self::SUCCESS_MESSAGE);
+        return $this;
     }
 }
